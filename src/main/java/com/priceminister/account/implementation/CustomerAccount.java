ecd66fb4a745c95bfb8974fba30e03b87e02102c
Michele Farruggio <michele.farruggio@gmail.com>
2014-01-06 14:23:36 +0100

    Update CustomerAccount.java

--- src/main/java/com/priceminister/account/implementation/CustomerAccount.java
+++ src/main/java/com/priceminister/account/implementation/CustomerAccount.java
@@ -4,20 +4,29 @@ import com.priceminister.account.*;
 
 
 public class CustomerAccount implements Account {
-
+	double customerBalance = 0;
+	
     public void add(Double addedAmount) {
-        // TODO Auto-generated method stub
+        customerBalance += addedAmount;
     }
 
     public Double getBalance() {
-        // TODO Auto-generated method stub
-        return null;
+    	Double balance = new Double(customerBalance);
+        
+    	return balance!=null?customerBalance:new Double(0.0);
     }
 
     public Double withdrawAndReportBalance(Double withdrawnAmount, AccountRule rule) 
     		throws IllegalBalanceException {
-        // TODO Auto-generated method stub
-        return null;
+    	
+        double resultingAccountBalance = customerBalance - withdrawnAmount;
+
+        if(rule.withdrawPermitted(resultingAccountBalance)) {
+    		return Math.ceil(resultingAccountBalance);
+    	}
+    	else {
+    		throw new IllegalBalanceException(resultingAccountBalance);
+    	}
     }
 
 }
